9e124671d83d97b898f73cb0234a72e63335fbbe
Maciej Królik <maciejkrolik@outlook.com>
2018-12-10 12:44:26 +0100

    Set up navigation component

---
+++ app/src/main/java/pl/expert/mobilewzr/TimetableFragment.kt
@@ -0,0 +1,17 @@
+package pl.expert.mobilewzr
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TimetableFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_timetable, container, false)
+    }
+}
