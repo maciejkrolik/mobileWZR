2c3083eb85e4a86c809774faa885bfbb4da99812
Maciej Królik <maciejkrolik@outlook.com>
2020-06-18 14:04:50 +0200

    Disable lecturers timetable feature, update gradle dependencies

--- app/src/main/java/pl/expert/mobilewzr/data/WZRService.kt
+++ app/src/main/java/pl/expert/mobilewzr/data/WZRService.kt
@@ -7,6 +7,10 @@ import retrofit2.http.Query
 interface WZRService {
 
     @GET(URLs.SUBJECTS_PATH)
-    suspend fun getSubjects(@Query("f1") groupId: String): List<Subject>
+    suspend fun getSubjects(
+        @Query("f1") groupId: String,
+        @Query("jp") params: String,
+        @Query("f2") number: Int = 4
+    ): List<Subject>
 
 }
